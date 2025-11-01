9b2c44e1207ef78da5d16b263a18de835f9d9ebd
ryder-11505 <99353@stpeters.qld.edu.au>
2025-11-01 11:12:30 +1000

    change to nextjs ssg

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'export',
+  distDir: 'docs',
+  assetPrefix: "/shooter-speed-calc/"
+};
+
+export default nextConfig;
